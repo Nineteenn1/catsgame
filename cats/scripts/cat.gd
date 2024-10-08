3da3a0d2dc897e0a48048c529cce2b125d684365
fxxsx <nineteen.yt123@gmail.com>
2024-10-08 16:04:36 +0200

    chasing fix, health points, lubudubu

--- cats/scripts/cat.gd
+++ cats/scripts/cat.gd
@@ -5,6 +5,8 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
+@onready var rat_node = get_parent().get_node("rat")
+
 func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
@@ -21,5 +23,14 @@ func _physics_process(delta: float) -> void:
 		velocity.y = directionY * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+
+
+func _input(InputEvent) -> void:
+	if Input.is_action_just_pressed("SHOOT"):
+		if rat_node:
+			rat_node.takeDamage(50)
+			print(rat_node.health)
+		
 
 	move_and_slide()
--- cats/scripts/rat.gd
+++ cats/scripts/rat.gd
@@ -3,10 +3,20 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 
+var health = 100
+
 @onready var cat_node = get_parent().get_node("cat")
 
-var cat_position = Vector2(0,0) 
-var cat_velocity = Vector2(0,0)
+@onready var cat_position = cat_node.get("position")
+@onready var cat_velocity = cat_node.get("velocity")
+
+func takeDamage(damage: int):
+	health -= damage
+
+func die():
+	# particle animation
+	queue_free()
+	pass
 
 func _process(delta: float) -> void:
 	cat_position = cat_node.get("position")
@@ -15,4 +25,11 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	velocity = global_position.direction_to(cat_position) * SPEED
 		
+	print("cat pos: " + str(cat_position.x) + ", " + str(cat_position.y)) 	
+	print("rat pos: " + str(position.x) + ", " + str(position.y)) 	
+		
+	if health == 0:
+		die()
+	
+	#if not (position == cat_position):
 	move_and_slide()
